cf65f29f443f06a643ed50b3ba0909523a7cc249
nonanik112 <METEHAN_1306@hotmail.com>
2020-06-24 22:37:13 +0300

    NodeJs_Deneme

---
+++ index.js
@@ -0,0 +1,6 @@
+const person = require('./person');
+
+
+const person1 = new person('Bilge Türk',35)
+
+person1.greeting();
---
+++ person.js
@@ -0,0 +1,19 @@
+//(function (exports,require,module, _filenamei, _dirname){
+
+//})
+
+//console.log(__dirname,__filename);
+
+class person {
+    constructor(name,age){
+          this.name = name;
+          this.age= age;
+    }
+
+
+    greeting(){
+        console.log(`My name is ${this.name}and I am ${this.age}`);
+    }
+}
+
+module.exports = person
